e0b8d01659e68df5e3cd2b51fd232e011e3e3a4a
Frode Kirkholt <fkirkholt@yahoo.no>
2021-04-18 10:13:06 +0200

    Move template folder

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="static/html")
 rand = random()
 
 @app.get("/", response_class=HTMLResponse)

3410def86dceb3678a07c9cd20edf5832c6b2fa6
Frode Kirkholt <fkirkholt@yahoo.no>
2021-06-09 14:21:35 +0200

    queries for finding metadata for oracle

--- expression.py
+++ expression.py
@@ -108,4 +108,60 @@ class Expression:
 
         return sql
         # todo:t Må ha autentisering på plass før denne kan lages
-            
+
+    def indexes(self):
+        if self.platform == 'oracle':
+            return """
+            select i.index_name, case uniqueness when 'NONUNIQUE' then 1 else 0 end as non_unique,
+                   lower(column_name) as column_name, column_position, i.table_name
+            from all_indexes i
+            join all_ind_columns col on col.index_name = i.index_name
+            where i.table_owner = ?
+            order by column_position
+            """
+    def pkeys(self):
+        if self.platform == 'oracle':
+            return """
+            SELECT cols.table_name, cols.column_name, cols.position as key_seq, cons.status, cons.owner
+            FROM all_constraints cons, all_cons_columns cols
+            WHERE cons.constraint_type = 'P'
+            AND cons.constraint_name = cols.constraint_name
+            AND cons.owner = cols.owner
+            AND cons.owner = ?
+            AND cols.table_name = nvl(?, cols.table_name)
+            ORDER BY cols.table_name, cols.position;
+            """
+    def fkeys(self):
+        if self.platform == 'oracle':
+            return """
+            SELECT a.column_name as fkcolumn_name, a.position,
+                   a.constraint_name as fk_name, a.table_name as fktable_name,
+                    c.owner, c.delete_rule,
+                    -- referenced pk
+                    c.r_owner as pktable_cat, c_pk.table_name as pktable_name,
+                    c_pk.constraint_name r_pk,
+                    ra.column_name pkcolumn_name
+            FROM all_cons_columns a
+                JOIN all_constraints c
+                ON a.owner = c.owner
+                AND a.constraint_name = c.constraint_name
+                JOIN all_constraints c_pk
+                ON c.r_owner = c_pk.owner
+                AND c.r_constraint_name = c_pk.constraint_name
+                JOIN all_cons_columns ra
+                ON ra.owner = c.owner
+                AND ra.constraint_name = c_pk.constraint_name
+                AND ra.position = a.position
+            WHERE c.constraint_type = 'R'
+            AND   a.owner = ?
+            ORDER BY a.position
+            """
+    def columns(self):
+        if self.platform == 'oracle':
+            return """
+            select lower(table_name) as table_name, lower(column_name) as column_name,
+                   data_type as type_name, data_length as column_size,
+                   case nullable when 'Y' then 1 else 0 end as nullable
+            from all_tab_columns
+            where owner = ? and table_name = nvl(?, table_name)
+            """

8c85a0ce63c1c9f5df3f7f9bae1fc986f7793ea4
Frode Kirkholt <fkirkholt@yahoo.no>
2022-05-07 16:27:34 +0200

    Use comma between display fields

--- column.py
+++ column.py
@@ -77,7 +77,7 @@ class Column:
                         # Only last pk column is used in display value,
                         # other pk columns are usually foreign keys
                         cols = [self.name+"."+col for col in index.columns if col not in ref_pk[0:-1]]
-                        field.view = " || ' - ' || ".join(cols)
+                        field.view = " || ', ' || ".join(cols)
                         if index.name.endswith("_sort_idx"):
                             break
 
